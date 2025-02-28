e38a3f1daa7e8018c6e743ff0ac6e4329733b524
Javohir Mirjalilov <uzbekfarzandi906@gmail.com>
2025-02-28 13:10:05 +0300

    added: first lab

--- PHP/lab-1/index.php
+++ PHP/lab-1/index.php
@@ -1,20 +1,44 @@
-<?php
-    class Cat {
-        private $name;
-        public $color;
-        public $weight;
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>241-3210 Mirjalilov Javohir</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="https://mospolytech.ru/local/templates/main/assets/img/logo.svg" alt="Логотип МосПолитеха">
+        </div>
+        <div class="title">
+            <h1>Hello, World!</h1>
+        </div>
+    </header>
 
-        function __construct(string $name, string $color, int $weight) {
-            $this->name = $name;
-            $this->color = $color;
-            $this->weight = $weight;
-        }
+    <main>
+        <?php
+        $message = "Hello, World";
+        echo "<p>$message</p>";
+        ?>
+    </main>
 
-        function getName() {
-            return $this->name;
-        }
-    }
+    <footer>
+    <?php
+        $randomElements = [
+            "Сегодня отличный день для изучения PHP!",
+            "Никогда не сдавайтесь!",
+            "Код — это поэзия.",
+            "Учитесь, пробуйте, ошибайтесь и снова учитесь.",
+            "PHP — это весело!",
+            "Случайная цитата: «Лучше сделать и пожалеть, чем не сделать и пожалеть.»",
+            "Вдохновение приходит во время работы.",
+            "Каждая проблема — это возможность научиться чему-то новому."
+        ];
 
-    $cat = new Cat("Barsik", "black", 3);
-    echo $cat->getName()."<br>";
-    // var_dump($cat);
+        $randomElement = $randomElements[array_rand($randomElements)];
+        echo "<p>$randomElement</p>";
+        ?>
+    </footer>
+</body>
+</html>
